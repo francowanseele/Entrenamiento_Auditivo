9fbc7a8ca2e5d2d09f11ca7405c20d81315ea0bd
franco.wanseele <franco.wanseele@gmail.com>
2021-05-16 12:37:23 -0300

    first

---
+++ app/screens/Student/Calification.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Calification() {
+    return (
+        <View>
+            <Text>Calificaciones...</Text>
+        </View>
+    );
+}
