0f3fa2d8988d27dfd6546e1b2ec511194eef65d9
martin <maitinn2@hotmail.com>
2021-05-16 16:57:30 -0300

    genera varios compases en el grafico

--- App.js
+++ App.js
@@ -11,7 +11,38 @@ export default function App() {
         <View style={styles.container}>
             <View style={styles.graficoContainer}>
                 <ScrollView horizontal={true} style={styles.scrollView}>
-                    <Graficar style={styles.grafico}  notasParam={'a/4, a/4, b/4, C4'} numeradorParam={4} denominadorParam={4} />
+                    <Graficar 
+                        style={styles.grafico}  
+                        figurasParam={ 
+                            [                             
+                            ["a/4","1"], 
+                            ["b/4","4"],
+                            ["c/4","4"],
+                            ["b/4","1"],
+                            ["b/4","1"],
+                            ["NuevoCompas"],
+                            ["a/4","2"], 
+                            ["b/4","2"],
+                            ["c/4","2"],
+                            ["b/4","2"],
+                            ["b/4","2"],
+                            ["NuevoCompas"],
+                            ["a/4","2"], 
+                            ["b/4","2"],
+                            ["c/4","2"],
+                            ["b/4","2"],
+                            ["NuevoCompas"],
+                            ["b/4","2"],
+                            ["a/4","1"], 
+                            ["b/4","4"],
+                            ["c/4","4"],
+                                                 
+                            ]
+                        }
+                        numeradorParam={4}
+                        denominadorParam={4}
+                        claveParam={'treble'} 
+                    />
                 </ScrollView>
             </View>            
         </View>
@@ -28,10 +59,12 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
     },
     graficoContainer:{
-        height:'35%',
+        paddingTop:30,
+        paddingHorizontal:5,
+        height:'40%',
     },
     scrollView:{
-        height:100
+        height:100,
     },
 
     grafcio:{
--- componentes/Graficar.js
+++ componentes/Graficar.js
@@ -1,144 +1,110 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-export default ( {notasParam,figurasParam,cantCompasesParam,numeradorParam, denominadorParam} ) =>{
+export default ( {figurasParam,cantCompasesParam,numeradorParam, denominadorParam, claveParam} ) =>{
 
-    const [notasPorCompas, setNotasPorCompas] = useState(notasParam);
     const [cantCompases, setcantCompases] = useState(cantCompasesParam);
     const [figuras, setfiguras] = useState(figurasParam); 
     const [denominador, setDenominador] = useState(denominadorParam);
     const [numerador, setNumerador] = useState(numeradorParam);
+    const [clave,setclave] = useState(claveParam);
 
+
+
+    const getFigurasyDuracion = () =>{
+      let res = '';
+      let compasActual =1;
+      for (let actual in figuras){
+        if ( figuras[actual] != 'NuevoCompas'){
+        res = res.concat('new Vex.Flow.StaveNote({ keys: ["'+figuras[actual].[0]+'"], duration: "'+figuras[actual].[1]+'" }),'+'\n')
+        }else 
+          if ( figuras[actual] == 'NuevoCompas'){ 
+          compasActual = compasActual +1;
+          res = res.concat(`     
+            ]; \n
+
+            Vex.Flow.Formatter.FormatAndDraw(context, staveMeasure`+(compasActual-1)+`, notesMeasure`+(compasActual-1)+`);
+            var staveMeasure`+compasActual+` = new Vex.Flow.Stave( \n
+              staveMeasure`+(compasActual-1)+`.width + staveMeasure`+(compasActual-1)+`.x, 
+              0,
+              400
+            );\n
+            staveMeasure`+compasActual+`.setContext(context).draw();\n
+            
+            // Notas dentro del compas
+            var notesMeasure`+compasActual+`= [
+            `)
+        }
+      }
+      res = res.concat(`]; \n
+        Vex.Flow.Formatter.FormatAndDraw(context, staveMeasure`+compasActual.toString()+`, notesMeasure`+compasActual.toString()+`);\n
+        `)
+      
+      return res;
+    }
+
+    const [Html, setHtml] = useState('')
+
+
+    useEffect(()=>{
+      setHtml(`    
+      <style>
+      .music-render {            
+          margin:  auto;
+          height: 1px;
+      }
+      </style>
+      <script src="https://unpkg.com/vexflow/releases/vexflow-debug.js"> 
+      </script>
+      <script>
+      function writeNote(pitch, rhythm){  //(tono,ritmo)
+        
+          VF = Vex.Flow;
+  
+          // Create an SVG renderer and attach it to the DIV element named "boo".
+          var div = document.getElementById("boo")
+          var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+          
+          // Size our SVG:
+          renderer.resize(2500, 100);
+          
+          // And get a drawing context:
+          var context = renderer.getContext();
+          
+          
+          // measure 1
+            var staveMeasure1 = new Vex.Flow.Stave(10, 0, 300);
+            staveMeasure1.addClef("`+clave+`").addTimeSignature('`+numerador+`/`+denominador+`').setContext(context).draw();
+          
+            var notesMeasure1 = [
+              `+getFigurasyDuracion()+`
+            
+          
+          
+      }
+      </script>
+          <div class="music-render" id="boo"/> 
+          <script>writeNote();</script>
+  `
+    )
+    console.log(Html)
+    },[])
     
 
 
-  return (    
+  return (
           <WebView 
-          source={{ html:  
-    `    
-        <style>
-        .music-render {            
-            margin: 30px auto;
-        }
-        </style>
-        <script src="https://unpkg.com/vexflow/releases/vexflow-debug.js"> 
-        </script>
-        <script>
-        function writeNote(pitch, rhythm){  //(tono,ritmo)
-          
-            VF = Vex.Flow;
-
-            // Create an SVG renderer and attach it to the DIV element named "boo".
-            var div = document.getElementById("boo")
-            var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-            
-            // Size our SVG:
-            renderer.resize(2500, 100);
-            
-            // And get a drawing context:
-            var context = renderer.getContext();
-            
-            
-            // measure 1
-              var staveMeasure1 = new Vex.Flow.Stave(10, 0, 300);
-              staveMeasure1.addClef("treble").addTimeSignature('`+numerador+`/`+denominador+`').setContext(context).draw();
-            
-              var notesMeasure1 = [
-                new Vex.Flow.StaveNote({ keys: ["a/4"], duration: "1" }),
-                new Vex.Flow.StaveNote({ keys: ["a/4"], duration: "2" }),
-                new Vex.Flow.StaveNote({ keys: ["e/4"], duration: "4" }),
-                new Vex.Flow.StaveNote({ keys: ["b/4"], duration: "8" }),
-              ];
-            
-              // Helper function to justify and draw a 4/4 voice
-              Vex.Flow.Formatter.FormatAndDraw(context, staveMeasure1, notesMeasure1);
-            
-            
-            
-            
-            
-              // measure 2 - Linea comienzo nuevo compas
-              var staveMeasure2 = new Vex.Flow.Stave(
-                staveMeasure1.width + staveMeasure1.x,
-                0,
-                400
-              );
-              staveMeasure2.setContext(context).draw();
-            
-            
-             // Notas dentro del compas
-              var notesMeasure2_part1 = [
-                new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "8" }),
-                new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "8" }),
-                new Vex.Flow.StaveNote({ keys: ["b/4"], duration: "8" }),
-                new Vex.Flow.StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "8" }),
-              ];
-            
-              var notesMeasure2_part2 = [
-                new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "8" }),
-                new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "8" }),
-                new Vex.Flow.StaveNote({ keys: ["b/4"], duration: "8" }),
-                new Vex.Flow.StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "8" }),
-              ];
-            
-              // create the beams for 8th notes in 2nd measure
-              var beam1 = new Vex.Flow.Beam(notesMeasure2_part1);
-              var beam2 = new Vex.Flow.Beam(notesMeasure2_part2);
-            
-              var notesMeasure2 = notesMeasure2_part1.concat(notesMeasure2_part2);
-            
-              Vex.Flow.Formatter.FormatAndDraw(context, staveMeasure2, notesMeasure2);
-            
-              // Render beams
-              beam1.setContext(context).draw();
-              beam2.setContext(context).draw();
-              
-              
-              
-              
-              // measure 3 -Linea comienzo de nuevo compas
-              var staveMeasure3 = new Vex.Flow.Stave( staveMeasure2.width  + staveMeasure2.x ,
-                0,
-                400
-              );
-              staveMeasure3.setContext(context).draw();
-              
-              
-              // measure 3   Lo que va dentro del compas
-              staveMeasure3.setContext(context).draw();
-            
-              var notesMeasure3 = [
-                new VF.StaveNote({clef: "treble", keys: ["e##/5"], duration: "8d" }).
-                  addAccidental(0, new VF.Accidental("##")).addDotToAll(),
-            
-                new VF.StaveNote({clef: "treble", keys: ["eb/5"], duration: "16" }).
-                  addAccidental(0, new VF.Accidental("b")),
-            
-                new VF.StaveNote({clef: "treble", keys: ["d/5", "eb/4"], duration: "h" }).
-                    addDot(0),
-            
-                new VF.StaveNote({clef: "treble", keys: ["c/5", "eb/5", "g#/5"], duration: "q" }).
-                  addAccidental(1, new VF.Accidental("b")).
-                  addAccidental(2, new VF.Accidental("#")).addDotToAll()
-              ];
-            
-              // Helper function to justify and draw a 4/4 voice
-              Vex.Flow.Formatter.FormatAndDraw(context, staveMeasure3, notesMeasure3);
-        }
-        </script>
-            <div class="music-render" id="boo"/> 
-            <script>writeNote();</script>
-    `
-    }} 
-    style={{ marginTop: 50,
-              alignSelf:'center',
-              width:3500, // aumentando este ancho logro aumentar el taman;o de las figuras
-              backgroundColor:'red'
-     }}
- />
-
+                  source={{ html: Html           
+            }} 
+            style={{
+                      alignSelf:'center',
+                      width:3000, // aumentando este ancho logro aumentar el taman;o de las figuras
+                      maxHeight:'100%',
+                      backgroundColor:'red'
+            }}
+            />
   );
 }
 
