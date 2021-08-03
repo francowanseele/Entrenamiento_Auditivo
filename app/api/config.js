9e9681f4b8936bda2c7a98d9ea4aad238863bb98
martinnogueira <maitinn2@hotmail.com>
2021-08-03 16:02:59 -0300

    nuevabranch

--- app/api/config.js
+++ app/api/config.js
@@ -1,2 +1,3 @@
-// export const basePath = 'http://10.0.2.2:3977/api';
-export const basePath = 'http://localhost:3977/api';
+export const basePath = 'http://10.0.2.2:3977/api';
+// export const basePath = 'http://192.168.1.8:3977/api';
+// export const basePath = 'http://localhost:3977/api';
