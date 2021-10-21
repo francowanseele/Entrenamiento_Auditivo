3bbae25043ba42adccd9d61e57921df05f79881d
franco.wanseele <franco.wanseele@gmail.com>
2021-10-21 20:07:46 -0300

    change in bathePath

--- app/api/config.js
+++ app/api/config.js
@@ -1,5 +1,5 @@
 //export const basePath = 'http://10.0.2.2:3977/api';
 
 
-//export const basePath = 'http://localhost:3977/api';
-export const basePath = 'https://aplicaciondictadosarmonicos.herokuapp.com/api';
+export const basePath = 'http://localhost:3977/api';
+//export const basePath = 'https://aplicaciondictadosarmonicos.herokuapp.com/api';
