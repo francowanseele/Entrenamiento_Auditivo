60ba4fa4eb60d44062ebdaa8f655627fdabc0c57
franco.wanseele <franco.wanseele@gmail.com>
2023-04-19 18:04:37 -0300

    trying to upgrade RN version

--- ios/Entrenamiento_Auditivo/AppDelegate.h
+++ ios/Entrenamiento_Auditivo/AppDelegate.h
@@ -1,8 +1,10 @@
-#import <React/RCTBridgeDelegate.h>
+// #import <React/RCTBridgeDelegate.h>
+#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+// @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+@interface AppDelegate : RCTAppDelegate
 
-@property (nonatomic, strong) UIWindow *window;
+// @property (nonatomic, strong) UIWindow *window;
 
 @end
